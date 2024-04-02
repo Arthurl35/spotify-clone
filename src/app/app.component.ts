2d4ac09f568b3eae07217803af9b30ea940c0af4
Arthur <arthur.levesque35@gmail.com>
2024-04-02 19:27:32 +0200

    import font-awesome library and testing a icon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'spotify-clone-front';
+
+  private faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesomeIcons();
+  }
+
+  private initFontAwesomeIcons(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
